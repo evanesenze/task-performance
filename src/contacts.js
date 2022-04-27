41d73b139ca9b56e7fd71db82efdce43834984c2
Ivan <64332115+evanesenze@users.noreply.github.com>
2022-04-28 00:16:50 +0500

    Performance

--- src/contacts.js
+++ src/contacts.js
@@ -1,26 +1,30 @@
 const contacts = document.getElementsByClassName("contacts")[0];
 const stickyHeader = document.getElementsByClassName("stickyHeader")[0];
+let timeout = null;
+let items = [];
 
 function addContacts() {
-  const fragment = document.createDocumentFragment();
-  for (let i = 0; i < 50000; i++) {
+  items = new Array(50000).fill(null).map((_, i) => {
     const child = document.createElement("div");
-    child.textContent = i;
+    child.textContent = Number(i);
     child.classList.add("contact");
-    fragment.appendChild(child);
-  }
-  contacts.appendChild(fragment);
+    return child;
+  });
+  contacts.append(...items);
 }
 
 contacts.addEventListener("scroll", (e) => {
-  const items = Array.from(contacts.getElementsByClassName("contact"));
-  const itemOffsets = items.map((item) => item.offsetTop);
-  const topItemIndex = itemOffsets.findIndex(
-    (offset) => contacts.scrollTop - offset <= -18
-  );
-  if (topItemIndex !== -1) {
-    stickyHeader.textContent = items[topItemIndex].textContent;
-  }
+  clearTimeout(timeout);
+  timeout = setTimeout(() => {
+    const itemOffsets = items.map((item) => item.offsetTop);
+    const topItemIndex = itemOffsets.findIndex(
+      (offset) => contacts.scrollTop - offset <= -18
+    );
+    if (topItemIndex !== -1) {
+      stickyHeader.textContent = items[topItemIndex].textContent;
+    }
+    timer = null;
+  });
 });
 
 addContacts();
